6cc645901fd5a3001bf61e0c15b2b82d3a83abad
David Marby <david@dmarby.se>
2017-08-02 16:05:42 +0200

    Update local bind with dynamic dns as well

--- ansible/roles/vyos/vyos_base_configuration/templates/dynamic_dns.sh
+++ ansible/roles/vyos/vyos_base_configuration/templates/dynamic_dns.sh
@@ -8,6 +8,15 @@ if [ $? == 0 ]
 then
   IP="$(run show interfaces ethernet eth1 | grep "inet " | awk -F ' ' '{print $4}')"
 
+  (
+  echo "server 127.0.0.1"
+  echo "zone home.dmarby.se"
+
+  echo "update delete home.dmarby.se A"
+  echo "update add home.dmarby.se 60 A ${IP}"
+  echo "send"
+  ) | /usr/bin/nsupdate
+
   curl -H "Authorization: Bearer {{ digitalocean_token }}" -H "Content-Type: application/json" \
     -d '{"data": "'${IP}'"}' \
     -X PUT "https://api.digitalocean.com/v2/domains/home.dmarby.se/records/24417816"
