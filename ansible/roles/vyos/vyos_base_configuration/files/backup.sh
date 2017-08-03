ff34c1cacadff960a94b22ffc8fdb1c5ce7fa40e
David Marby <david@dmarby.se>
2017-08-03 09:49:49 +0200

    Fix vrrp hooks preventing correct boot

--- ansible/roles/vyos/vyos_base_configuration/files/backup.sh
+++ ansible/roles/vyos/vyos_base_configuration/files/backup.sh
@@ -1,5 +1,14 @@
 #!/bin/vbash
 
+# Disable the interface immidately so that we don't break the master on boot
+ip link set eth1 down
+
+# Wait until vyos have started
+until systemctl is-active multi-user.target
+do
+  true
+done
+
 # Ensure that we have the correct group or we'll corrupt the configuration
 if [ "$(id -g -n)" != 'vyattacfg' ] ; then
 exec sg vyattacfg -c "/bin/vbash $(readlink -f $0) $@"
@@ -7,16 +16,10 @@ fi
 
 source /opt/vyatta/etc/functions/script-template
 
-# Wait until vyos have started
-until systemctl is-active multi-user.target
-do
-  sleep 5
-done
-
-# Disable the WAN interface
-# Make sure that this is not saved to the boot config, or the configuration won't load on boot.
+# Disable DHCP server
+# Make sure that this is not saved to the boot config, or the configuration might not load on boot.
 configure
-set interfaces ethernet eth1 disable
+set service dhcp-server disabled 'true'
 commit
 
 exit
