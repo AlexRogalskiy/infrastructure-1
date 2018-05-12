38d1439f59d549668f0700ecbe0ebf0690373d3e
David Marby <david@dmarby.se>
2018-05-13 01:32:59 +0200

    Make sonarr use terraform Use the develop repo for sonarr, since we use the develop branch for the autoupdater anyway Get mono from the bionic-stable repo Use smb to connect to the storage server

---
+++ terraform/sonarr.tf
@@ -0,0 +1,37 @@
+resource "vsphere_virtual_machine" "sonarr" {
+  name             = "sonarr"
+  resource_pool_id = "${vsphere_compute_cluster.dmarby_cluster.resource_pool_id}"
+  datastore_id     = "${data.vsphere_datastore.esxi2.id}"
+
+  num_cpus = 4
+  memory   = 2048
+  guest_id = "${data.vsphere_virtual_machine.ubuntu-18_04.guest_id}"
+
+  network_interface {
+    network_id   = "${data.vsphere_network.vpn.id}"
+    adapter_type = "${data.vsphere_virtual_machine.ubuntu-18_04.network_interface_types[0]}"
+  }
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.ubuntu-18_04.id}"
+
+    customize {
+      network_interface {}
+
+      linux_options {
+        host_name = "sonarr"
+        domain    = "home.dmarby.se"
+      }
+    }
+  }
+
+  # Prevent terraform from recreating VMs when we update the template
+  lifecycle {
+    ignore_changes = ["clone.0.template_uuid", "disk.0"]
+  }
+}
