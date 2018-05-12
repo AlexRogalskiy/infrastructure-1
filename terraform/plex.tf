39ad9aee8f26d59e9e9f63571fccc0021d09fa39
David Marby <david@dmarby.se>
2018-05-12 23:57:21 +0200

    Make plex use the new role for mounting the storage over smb, move the transcode sync directory to a network share

--- terraform/plex.tf
+++ terraform/plex.tf
@@ -1,17 +1,17 @@
 resource "vsphere_virtual_machine" "plex" {
   name             = "plex"
-  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
-  datastore_id     = "${data.vsphere_datastore.esxi.id}"
+  resource_pool_id = "${vsphere_compute_cluster.dmarby_cluster.resource_pool_id}"
+  datastore_id     = "${data.vsphere_datastore.esxi1.id}"
 
   num_cpus = 16
   memory   = 4096
   guest_id = "${data.vsphere_virtual_machine.ubuntu-18_04.guest_id}"
 
   network_interface {
-    network_id = "${data.vsphere_network.vm.id}"
-    adapter_type = "${data.vsphere_virtual_machine.ubuntu-18_04.network_interface_types[0]}"
+    network_id     = "${data.vsphere_network.vm.id}"
+    adapter_type   = "${data.vsphere_virtual_machine.ubuntu-18_04.network_interface_types[0]}"
     use_static_mac = true
-    mac_address = "00:50:56:a7:62:a6" # Set to a static mac address for DHCP
+    mac_address    = "00:50:56:a7:62:a6"                                                       # Set to a static mac address for DHCP
   }
 
   disk {
@@ -37,6 +37,3 @@ resource "vsphere_virtual_machine" "plex" {
     ignore_changes = ["clone.0.template_uuid", "disk.0"]
   }
 }
-
-# TODO: Move Sync directory to network share
-# TODO: Move temporary transcode directory to network share maybe? Idk.
