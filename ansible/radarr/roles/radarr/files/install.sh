219c79e2f61de65be14798afa222638489b34daf
David Marby <david@dmarby.se>
2018-05-13 01:51:16 +0200

    Use terraform to set up radarr Move to a the 18.04 mono repo Use smb to connect to the storage server

--- ansible/radarr/roles/radarr/files/install.sh
+++ ansible/radarr/roles/radarr/files/install.sh
@@ -3,3 +3,4 @@
  radarr_tag=$(curl -sX GET "https://api.github.com/repos/Radarr/Radarr/releases" | awk '/tag_name/{print $4;exit}' FS='[""]')
  curl -o /tmp/radar.tar.gz -L	"https://github.com/Radarr/Radarr/releases/download/${radarr_tag}/Radarr.develop.${radarr_tag#v}.linux.tar.gz"
  tar ixzf /tmp/radar.tar.gz -C /opt/radarr --strip-components=1
+ chown -R radarr:radarr /opt/radarr
