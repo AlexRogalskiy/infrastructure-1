65a6f6b8cab7cb3548d88f11e93fe0359c817574
David Marby <david@dmarby.se>
2018-09-12 16:40:07 +0200

    Update VyOS packer config

--- packer/vyos-1.2/scripts/base.sh
+++ packer/vyos-1.2/scripts/base.sh
@@ -1,8 +1,8 @@
 #!/bin/vbash
 source /opt/vyatta/etc/functions/script-template
 
-# Set up DHCP on default interface
-set interface ethernet eth0 address dhcp
+# Set up DHCP on default interface on our servers VLAN
+set interface ethernet eth0 vif 20 address dhcp
 
 # Enable and configure SSH
 set service ssh
