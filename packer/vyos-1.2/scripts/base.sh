8b598e2b98cbdff50780448c03a03e04ad997bc0
David Marby <david@dmarby.se>
2018-09-16 11:51:58 +0200

    Stop adding debian jessie repositories to vyos

--- packer/vyos-1.2/scripts/base.sh
+++ packer/vyos-1.2/scripts/base.sh
@@ -11,10 +11,5 @@ set service ssh disable-host-validation
 # Delete default vyos user
 delete system login user vyos
 
-# Add Debian Jessie Repository
-set system package repository jessie components 'main contrib non-free'
-set system package repository jessie distribution 'jessie'
-set system package repository jessie url 'http://httpredir.debian.org/debian/'
-
 commit
 save
