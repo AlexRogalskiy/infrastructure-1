31eb7e321201ed3d1df9f984400e2471d4a9dfbf
David Marby <david@dmarby.se>
2020-04-11 16:38:56 +0200

    Add homebridge vm

---
+++ terraform/homebridge-apartment.tf
@@ -0,0 +1,41 @@
+resource "vsphere_virtual_machine" "homebridge-apartment" {
+  name             = "homebridge-apartment"
+  tags             = [vsphere_tag.homebridge.id]
+  resource_pool_id = vsphere_compute_cluster.apartment_cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.esxi-apartment.id
+
+  num_cpus = 2
+  memory   = 2048
+  guest_id = data.vsphere_virtual_machine.ubuntu-18_04.guest_id
+
+  network_interface {
+    network_id     = vsphere_distributed_port_group.apartment_servers.id
+    adapter_type   = data.vsphere_virtual_machine.ubuntu-18_04.network_interface_types[0]
+    use_static_mac = true
+    mac_address    = "00:50:56:a7:40:c3"
+  }
+
+  disk {
+    label = "disk0"
+    size  = 750
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.ubuntu-18_04.id
+
+    customize {
+      network_interface {
+      }
+
+      linux_options {
+        host_name = "homebridge-apartment"
+        domain    = "home.dmarby.se"
+      }
+    }
+  }
+  # Prevent terraform from recreating VMs when we update the template
+  lifecycle {
+    ignore_changes = [clone.0.template_uuid, disk.0]
+  }
+}
+
